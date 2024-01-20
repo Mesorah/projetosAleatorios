b97e041a2403643f096798f633ef684fd64010b1
Mesorah <gabrielcambara123@gmail.com>
2024-01-20 10:35:52 -0300

    Update main.py

--- round_game_overhaul/main.py
+++ round_game_overhaul/main.py
@@ -7,7 +7,7 @@ def main():
 
 def attributes():
     health_player = 10
-    health_boss = 15
+    health_boss = 25
 
     return health_player, health_boss
 
@@ -28,20 +28,22 @@ def cure_potion(health_player, potion, cure_potion_player, took_potion):
     return health_player, potion, cure_potion_player, took_potion
 
 def player_attack(health_boss):
+    global player_situation
     chance_of_damage_player = randint(1, 5)
 
     if chance_of_damage_player != 5:
         health_boss -= 3
         print(f'boss health {health_boss}')
-        attack_situation = True
+        player_situation = True
 
     elif chance_of_damage_player == 5:
         print('you missed')
-        attack_situation = False
+        player_situation = False
 
-    return health_boss, attack_situation
+    return health_boss
 
 def boss_attack(health_player):
+    global boss_situation
     chance_of_damage_boss = randint(1,5)
 
     if chance_of_damage_boss != 5:
@@ -53,7 +55,7 @@ def boss_attack(health_player):
         print('he missed')
         boss_situation = False
 
-    return health_player, boss_situation
+    return health_player
 
 def critical_player(health_boss):
     health_boss -= 6
@@ -64,27 +66,33 @@ def critical_player(health_boss):
 def critical_boss(health_player):
     health_player -= 8
     print(f'he did a critical and did 8 damage you life {health_player}')
+
     return health_player
 
-
 def super_player(health_boss):
-    health_boss, attack_situation = player_attack(health_boss)
-    health_player, boss_situation = boss_attack(health_player)
-    super_list = []
-    if attack_situation:
+    
+    if player_situation:
         super_list.append(1)
-    elif not attack_situation:
+    elif not player_situation:
         super_list.clear()
     elif boss_situation:
         super_list.clear()
 
     if super_list.count(1) == 3:
         health_boss -= 7
+        super_list.clear()
+        print(f'did you use super | life boss: {health_boss}')
 
-    return health_boss, health_player
+    print(f'{super_list.count(1)} / 3 to use super')
+
+    return health_boss
 
 def match():
     main()
+
+    global super_list
+    super_list = []
+
     health_player, health_boss = attributes()
     potion = 3
     tot = 0
@@ -113,18 +121,20 @@ def match():
                 if critical_chance_player == 1:
                     health_boss = critical_player(health_boss)
                 else:
-                    health_boss, _ = player_attack(health_boss)
+                    health_boss = player_attack(health_boss)
+                    health_boss = super_player(health_boss)
 
             elif rounds == 2:
                 critical_chance_boss = randint(1, 10)
                 if critical_chance_boss == 1:
                     health_player = critical_boss(health_player)
                 else:
-                    health_player, _ = boss_attack(health_player)
+                    health_player  = boss_attack(health_player)
 
             if health_player <= 6 and potion > 0:
                 cure_potion_player = input('press 1 to attack or 2 to drink a healing potion: ')
                 health_player, potion, cure_potion_player, took_potion = cure_potion(health_player, potion, cure_potion_player, took_potion)
+                print()
 
                 if took_potion:
                     print(f'you recovered 4 life: {health_player}')
@@ -141,5 +151,4 @@ def match():
         if health_boss <= 0:
             print('you win')
             break
-
 match()
